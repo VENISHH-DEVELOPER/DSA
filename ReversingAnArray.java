90e8571220c8dfe1c09420e311924218b32e2f61
VENISHH-DEVELOPER <fsdvenishh@gmail.com>
2026-06-21 22:39:46 +0530

    Reversing an element in an array

---
+++ ReversingAnArray.java
@@ -0,0 +1,14 @@
+public class ReversingAnArray {
+    public static void main(String[] args) {
+        
+        int[] a={20,4,48, 50, 47};
+        
+        for(int i=a.length-1;i>=0;i--)
+        {
+            System.out.print(a[i] +" ");
+        }
+    
+    
+    
+    }
+}
